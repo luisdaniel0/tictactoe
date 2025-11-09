ed84057f2b15a6b6507fe37ec45f1c32bc845dff
Luis Sanchez <luisanchezdh@gmail.com>
2025-11-09 00:50:51 -0500

    basic mechanics for the game are implemented, currentPlayer changes after every cell click from X to O, checks if cell is empty or not before updating gameboard/cell

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 const cells = document.querySelectorAll(".cell");
 let currentPlayer = "X";
-cells.forEach((cell) => {
-  cell.addEventListener("click", (e) => {
-    const col = Number(e.target.dataset.col);
-    const row = Number(e.target.dataset.row);
-    console.log(row, col);
-    gameBoard[row][col] = currentPlayer;
-    console.log(gameBoard);
-  });
-});
 
 const gameBoard = [
   ["", "", ""],
   ["", "", ""],
   ["", "", ""],
 ];
+
+cells.forEach((cell) => {
+  cell.addEventListener("click", (e) => {
+    const col = Number(e.target.dataset.col);
+    const row = Number(e.target.dataset.row);
+    if (gameBoard[row][col]) {
+      return;
+    } else {
+      gameBoard[row][col] = currentPlayer;
+      cell.textContent = currentPlayer;
+      currentPlayer = currentPlayer === "X" ? "O" : "X";
+      console.log(gameBoard);
+    }
+  });
+});
