f30ea156ca259bce04b4282dd8fb5b300ed71828
Luis Sanchez <luisanchezdh@gmail.com>
2025-11-10 23:05:31 -0500

    implemented checkWinner function. looped over all win conditions stored all three positions into variables and added a conditional

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const cells = document.querySelectorAll(".cell");
 let currentPlayer = "X";
 
 const gameBoard = [
+  //two dimensional array
   ["", "", ""],
   ["", "", ""],
   ["", "", ""],
@@ -19,6 +20,10 @@ cells.forEach((cell) => {
       cell.textContent = currentPlayer;
       currentPlayer = currentPlayer === "X" ? "O" : "X";
       console.log(gameBoard);
+    }
+    const winner = checkWinner();
+    if (winner) {
+      alert("Game over!");
     }
   });
 });
@@ -66,4 +71,37 @@ const checkWinner = () => {
       [0, 2],
     ],
   ];
+
+  for (let i = 0; i < winConditions.length; i++) {
+    console.log(winConditions[i]);
+    const positionOne =
+      gameBoard[winConditions[i][0][0]][winConditions[i][0][1]];
+    const positionTwo =
+      gameBoard[winConditions[i][1][0]][winConditions[i][1][1]];
+    const positionThree =
+      gameBoard[winConditions[i][2][0]][winConditions[i][2][1]];
+    if (
+      positionOne === positionTwo &&
+      positionTwo === positionThree &&
+      positionOne !== ""
+    ) {
+      return positionOne;
+    }
+  }
+  return null;
+  // winConditions.forEach((winCon) => {
+  //   console.log(winCon);
+  //   const positionOne = gameBoard[winCon[0][0]][winCon[0][1]];
+  //   const positionTwo = gameBoard[winCon[1][0]][winCon[1][1]];
+  //   const positionThree = gameBoard[winCon[2][0]][winCon[2][1]];
+  //   if (
+  //     positionOne === positionTwo &&
+  //     positionTwo === positionThree &&
+  //     positionOne !== ""
+  //   ) {
+  //     return positionOne;
+  //   } else {
+  //     return null;
+  //   }
+  // });
 };
